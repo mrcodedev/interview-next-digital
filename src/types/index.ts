0923a9cf867ea6ffe4bc16dbbd945fca3e33fa56
MrCodeDev <22279904+mrcodedev@users.noreply.github.com>
2026-05-19 16:49:36 +0200

    feat(multiple): tests, components, ci/cd...

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,6 +3,3 @@ export type { User } from "./user";
 export type { Album } from "./album";
 export type { Photo } from "./photo";
 export type { Todo } from "./todo";
-
-// Compatibility alias for older naming.
-export type { Todo as ToDo } from "./todo";
